9a8556f8ace4cea20b9955ec52a67248a4b440a3
Umamaheswara.Reddy <maheshreddy3330@gmail.com>
2022-09-09 19:42:13 +0530

    Refactored the code

---
+++ src/test/java/TestRunner/test.java
@@ -0,0 +1,11 @@
+package TestRunner;
+
+public class test {
+
+	public static void main(String[] args) {
+		 String product =   System.getProperty("user.dir");
+	    System.out.println("product"+product);
+
+	}
+
+}
